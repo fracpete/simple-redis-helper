a40c319017a0ed161394294f0f54029cea78ff38
Peter Reutemann <fracpete@gmail.com>
2025-11-13 10:17:48 +1300

    removed "License" classifier (deprecated)

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     url="https://github.com/fracpete/redis_helper",
     classifiers=[
         'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
         'Topic :: Scientific/Engineering',
         'Programming Language :: Python :: 3',
     ],
